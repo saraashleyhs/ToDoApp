ced433f107a37f8f967981501a3e9978b1300048
saraashleyhs <sara.ashleyhs@gmail.com>
2019-09-30 19:03:12 -0500

    Finishing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todos';
+  todoList: any [] = [];
+  todoTitle: string;
+
+  ngOnInit() {
+    this.todoTitle = '';
+    this.todoList = [
+      // example of how to make an item in todo list
+      { title: 'Install Angular CLI', isDone: false },
+    ]; 
+  }
+  deleteTodo(todo:any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
+  addTodo():void{
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false
+    });
+  }
 }
+
