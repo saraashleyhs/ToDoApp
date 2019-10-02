7fb7e14883a202d489ab773a8607faedde4cc93f
saraashleyhs <sara.ashleyhs@gmail.com>
2019-10-02 18:14:06 -0500

    Saving before class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { TodoService } from './services/todo.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfirmationModalComponent
+    ConfirmationModalComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
